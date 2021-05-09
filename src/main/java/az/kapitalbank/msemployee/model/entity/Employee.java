0f6b459c5ea1cfa43b7899378df3223c4020a05d
kazimovae <=>
2021-05-10 00:06:34 +0400

    create Employee table

---
+++ src/main/java/az/kapitalbank/msemployee/model/entity/Employee.java
@@ -0,0 +1,27 @@
+package az.kapitalbank.msemployee.model.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "EMPLOYEE")
+public class Employee {
+    @Id
+    @GeneratedValue
+    private int empId;
+    private String name;
+    private String surname;
+    private String email;
+    private String organization;
+    private String position;
+
+}
