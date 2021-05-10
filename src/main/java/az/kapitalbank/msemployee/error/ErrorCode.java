0b3b9cacf9eb0d6a7084a708a3e9cad38509a51f
kazimovae <=>
2021-05-10 16:47:32 +0400

    handle ExpiredJwtException

--- src/main/java/az/kapitalbank/msemployee/error/ErrorCode.java
+++ src/main/java/az/kapitalbank/msemployee/error/ErrorCode.java
@@ -1,14 +1,11 @@
 package az.kapitalbank.msemployee.error;
 
-import org.springframework.http.HttpStatus;
-
 public interface ErrorCode {
-    String USER_NOT_FOUND = "USER_NOT_FOUND";
-    String INVALID_HEADER = "INVALID_TOKEN";
+    String INVALID_TOKEN = "INVALID_TOKEN";
     String INVALID_LOGIN_DETAILS = "INVALID_LOGIN_DETAILS";
     String INTERNAL_SERVER_ERROR = "INTERNAL_SERVER_ERROR";
     String EMPLOYEE_NOT_FOUND = "EMPLOYEE_NOT_FOUND";
     String DUPLICATE_RECORD = "DUPLICATE_RECORD";
-    String INVALID_REQUEST = "INVALID_REQUEST";
-
+    String METHOD_NOT_SUPPORTED = "METHOD NOT SUPPORTED";
+    String VALIDATION_ERROR = "VALIDATION_ERROR";
 }
--- src/main/java/az/kapitalbank/msemployee/error/GlobalExceptionHandler.java
+++ src/main/java/az/kapitalbank/msemployee/error/GlobalExceptionHandler.java
@@ -1,11 +1,17 @@
 package az.kapitalbank.msemployee.error;
 
 
+import io.jsonwebtoken.ExpiredJwtException;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @Slf4j
@@ -33,5 +39,32 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
         return new ResponseEntity<>(response, HttpStatus.NOT_FOUND);
     }
 
+    @ExceptionHandler(ExpiredJwtException.class)
+    public ResponseEntity<ErrorResponse> processExpiredJwtException(ExpiredJwtException ex) {
+        ErrorResponse response = new ErrorResponse(ErrorCode.INVALID_TOKEN,ex.getMessage());
+        log.error("ExpiredJwtException , error:{}, devMessage:{}", response, ex.getMessage());
+        return new ResponseEntity<>(response, HttpStatus.NOT_FOUND);
+    }
 
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResponse> handleInternalServerErrors(Exception ex) {
+        ErrorResponse response = new ErrorResponse(ErrorCode.INTERNAL_SERVER_ERROR, "Something went wrong");
+        log.error("Error unexpected internal server, responseBody:{},exception:{}", response, ex);
+        return new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+
+    @Override
+    protected ResponseEntity<Object> handleHttpRequestMethodNotSupported(HttpRequestMethodNotSupportedException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+        ErrorResponse response = new ErrorResponse(ErrorCode.METHOD_NOT_SUPPORTED, ex.getMessage());
+        log.error("Method not supported,exception:{}", ex.getMessage());
+        return new ResponseEntity<>(response, headers, HttpStatus.BAD_REQUEST);
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleHttpMessageNotReadable(HttpMessageNotReadableException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+        ErrorResponse errorResponse = new ErrorResponse(ErrorCode.VALIDATION_ERROR, ex.getMessage());
+        log.info("Http message not readable error:{},response:{}", ex.getMessage(), errorResponse);
+        return new ResponseEntity<>(errorResponse, headers, status);
+    }
 }
