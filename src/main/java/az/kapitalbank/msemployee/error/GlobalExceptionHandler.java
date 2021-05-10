318619e1c3fe4ecfe15d2a94456d6f03af95dc54
kazimovae <=>
2021-05-10 12:52:35 +0400

    add ExceptionHandler

---
+++ src/main/java/az/kapitalbank/msemployee/error/GlobalExceptionHandler.java
@@ -0,0 +1,11 @@
+package az.kapitalbank.msemployee.error;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@Slf4j
+@RestControllerAdvice
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+
+}
