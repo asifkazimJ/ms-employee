d7d94449bd2fdecace0717073f452fb6994fad81
kazimovae <=>
2021-05-10 21:23:17 +0400

    add Swagger and Actuator

--- src/main/java/az/kapitalbank/msemployee/config/SecurityConfig.java
+++ src/main/java/az/kapitalbank/msemployee/config/SecurityConfig.java
@@ -41,9 +41,24 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         auth.userDetailsService(userDetailsService);
     }
 
+    private static final String[] AUTH_WHITELIST = {
+            // -- Swagger UI v2
+            "/v2/api-docs",
+            "/swagger-resources",
+            "/swagger-resources/**",
+            "/configuration/ui",
+            "/configuration/security",
+            "/swagger-ui.html",
+            "/webjars/**",
+            // -- Swagger UI v3 (OpenAPI)
+            "/v3/api-docs/**",
+            "/swagger-ui/**",
+            "login"
+            // other public endpoints of your API may be appended to this array
+    };
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable().authorizeRequests().antMatchers("/login")
+        http.csrf().disable().authorizeRequests().antMatchers(AUTH_WHITELIST)
                 .permitAll().anyRequest().authenticated().and()
                 .exceptionHandling().and().sessionManagement()
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
